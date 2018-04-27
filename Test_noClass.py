c6b6bcafcccde9b6c6c3912a768f24156d914721
Sreeharsha Sura <sreeharsha491@gmail.com>
2018-04-26 22:07:58 -0500

    Test_initial_commit: no classes used, some errors corrected needs modification

--- Test_noClass.py
+++ Test_noClass.py
@@ -9,43 +9,43 @@ counts = 1000000  # Number of simulations
 
 # modelling for 1,000,000 population
 for i in range(0, counts):
-    ll_cc = 45000 * 20  # a1: Alcohol-atributable life years lost to chronic conditions
-    ll_cc = uniform(0.80 * ll_cc, 1.20 * ll_cc)  # a1: Range: =/- 20%
-    ll_ac = 45000 * 10  # a2: Alcohol-atributable life years lost to acute conditions
-    ll_ac = uniform(0.80 * ll_ac, 1.20 * ll_ac)  # a2: Range: =/- 20%
-    m_cc = 40000 * 20  # a3: Alcohol-atributable morbidity-related QALYs lost from chronic conditions
-    m_cc = uniform(0.60 * m_cc, 1.40 * m_cc)  # a3: Range: =/- 40%
-    m_ac = 40000 * 10  # a4: Alcohol-atributable morbidity-related QALYs lost from acute conditions
-    m_ac = uniform(0.60 * m_ac, 1.40 * m_ac)  # a4: Range: =/- 40%
-    tot_ll = ll_cc + ll_ac + m_cc + m_ac  # Total alcohol-attributable QALYs lost
-    d_sc = uniform(0.05, 0.25)  # Delivery of screening and counseling
-    a_sc = uniform(0.8, 0.95)  # Adherence with screening
+    ll_cc = 45000 * 20  # ll_cc: Alcohol-atributable life years lost to chronic conditions
+    ll_cc = uniform(0.80 * ll_cc, 1.20 * ll_cc)  # ll_cc: Range: =/- 20%
+    ll_ac = 45000 * 10  # ll_ac: Alcohol-atributable life years lost to acute conditions
+    ll_ac = uniform(0.80 * ll_ac, 1.20 * ll_ac)  # ll_ac: Range: =/- 20%
+    m_cc = 40000 * 20  # m_cc: Alcohol-atributable morbidity-related quality added life(QAL years) lost from chronic conditions
+    m_cc = uniform(0.60 * m_cc, 1.40 * m_cc)  # m_cc: Range: =/- 40%
+    m_ac = 40000 * 10  # m_ac: Alcohol-atributable morbidity-related QAL lost from acute conditions
+    m_ac = uniform(0.60 * m_ac, 1.40 * m_ac)  # m_ac: Range: =/- 40%
+    tot_ll = ll_cc + ll_ac + m_cc + m_ac  # tot_ll: Total alcohol-attributable QAL lost
+    d_sc = uniform(0.05, 0.25)  # d_sc: Delivery of screening and counseling
+    a_sc = uniform(0.8, 0.95)  # a_sc: Adherence with screening
 
-    e_sc = uniform(0.10, 0.35)  # Effectiveness of counseling at changing behaviour
-    e_cc = uniform(0.75, 1)  # Efficacy of behaviour change at reducing chronic conditions
-    e_ac = uniform(0.10, 0.50)  # Efficacy of behaviour change at reducing acute conditions
+    e_sc = uniform(0.10, 0.35)  # e_sc: Effectiveness of counseling at changing behaviour
+    e_cc = uniform(0.75, 1)  # e_cc: Efficacy of behaviour change at reducing chronic conditions
+    e_ac = uniform(0.10, 0.50)  # e_ac: Efficacy of behaviour change at reducing acute conditions
     e_w = 1 / tot_ll * (e_cc * (ll_ac + m_ac) + e_ac * (
-                ll_cc + m_cc))  # Weighted efficacy of behaviour change at reducing total alcohol-attributable QALYs lost
-    p_ll = tot_ll / (1 - d_sc * e_sc * e_w)  # Predicted alcohol-attributable QALYs lost
-    gain_ql_alc = p_ll * a_sc * e_sc * e_w  # QALYS gained, CPB
+                ll_cc + m_cc))  # e_w: Weighted efficacy of behaviour change at reducing total alcohol-attributable QAL lost
+    p_ll = tot_ll / (1 - d_sc * e_sc * e_w)  # p_ll: Predicted alcohol-attributable QAL lost
+    gain_ql_alc = p_ll * a_sc * e_sc * e_w  # gain_ql_alc: QAL gained, Clinically preventable
 
     alc.append(gain_ql_alc)
 
 # modelling for 1,000,000 population
 for i in range(0, counts):
-    sm = uniform(397500, 510000)  # a: Number of ever smokers in birth-cohort of 1,000,000
-    q_sm = 5.65  # b: Average gains in life expentancy for one quit
+    sm = uniform(397500, 510000)  # sm: Number of ever smokers in birth-cohort of 1,000,000
+    q_sm = 5.65  # q_sm: Average gains in life expentancy for one quit
     q_sm = uniform(0.75 * q_sm, 1.25 * q_sm)
-    sm_il = 177266  # c: QALYs lost to smoking atributable illness in birth cohort
+    sm_il = 177266  # sm_il: QAL lost to smoking atributable illness in birth cohort
     sm_il = uniform(0.5 * sm_il, 1.5 * sm_il)
-    per_sm = sm_il / sm  # d: QALYs lost to smoking-attributable illnesss per smoker
-    f_sm = uniform(0.47, 0.57)  # e: Portion of ever-smokers who are former smokers
-    rel_sm = uniform(0.20, 0.56)  # f: Relative risk of SA disease for former smokers compared to current ones
-    m_sm = per_sm / (f_sm * rel_sm + (1 - f_sm))  # g: QALYs lost from smoking morbidity percontinuing smoker
-    a_m = m_sm - m_sm * rel_sm  # h: QALYs saved from avoided morbidity per smoker quit
-    t_sc = uniform(0.05, 0.25)  # Delivery of screening and counseling
-    eff = 0.030  # j: Long-term effectiveness of repeated counseling in inducing quits
-    p_burden = sm * (q_sm + a_m) * eff  # k: Clinically preventable burden (QALYs saved)
+    per_sm = sm_il / sm  # per_sm: QAL lost to smoking-attributable illnesss per smoker
+    f_sm = uniform(0.47, 0.57)  # f_sm: Portion of ever-smokers who are former smokers
+    rel_sm = uniform(0.20, 0.56)  # rel_sm: Relative risk of SA disease for former smokers compared to current ones
+    m_sm = per_sm / (f_sm * rel_sm + (1 - f_sm))  # m_sm: QAL lost from smoking morbidity percontinuing smoker
+    a_m = m_sm - m_sm * rel_sm  # a_m: QAL saved from avoided morbidity per smoker quit
+    t_sc = uniform(0.05, 0.25)  # t_sc: Delivery of screening and counseling
+    eff = 0.030  # eff: Long-term effectiveness of repeated counseling in inducing quits
+    p_burden = sm * (q_sm + a_m) * eff  # p_burden: Clinically preventable burden (QAL saved)
     tob.append(p_burden)
 
 # Sort and Calculate Confidence Intervals
@@ -60,9 +60,9 @@ upper_limit2 = sorted_tob[int(0.975 * counts)]
 # Calculate Mean and Standard Deviation
 print("\n\nQuality added life years gained in alcohol cohort")
 print("Mean for life years gained (CPB): " + str(int(np.mean(sorted_alc))))
-print("Standard deviation for life years gained (CPB): " + str(int(np.std(Lsorted_alc))))
+print("Standard deviation for life years gained (CPB): " + str(int(np.std(sorted_alc))))
 print("Confidence interval: [" + str(int(lower_limit1)) + ", " + str(int(upper_limit1)) + "]")
-print("Number of simulations: " + str(len(Lsorted_alc)))
+print("Number of simulations: " + str(len(sorted_alc)))
 
 # Calculate Mean and Standard Deviation
 print("\n\nQuality added life years gained in smoking cohort")
@@ -80,12 +80,12 @@ lowerlimit_alc = final_alc[int(0.025 * counts)]
 upperlimit_alc = final_alc[int(0.975 * counts)]
 
 # Calculate Mean and Standard Deviation
-print("Mean quality years gained (CPB): " + str((np.mean(Lfinal_alc))))
-print("Standard deviation (CPB): " + str((np.std(Lfinal_alc))))
+print("Mean quality years gained (CPB): " + str((np.mean(final_alc))))
+print("Standard deviation (CPB): " + str((np.std(final_alc))))
 print("Confidence interval: [" + str((lowerlimit_alc)) + ", " + str((upperlimit_alc)) + "]")
-print("Number of simulations: " + str(len(Lfinal_alc)))
+print("Number of simulations: " + str(len(final_alc)))
 
-# Calculate the QALYs gained by intervention
+# Calculate the QAL years gained by intervention
 print("\n\nQuality_per_intervention_tobacco")
 NI = int(t_sc * 1000000)
 print("Number of interventions in the cohort: ", NI)
@@ -94,16 +94,16 @@ lowerlimit_tob = final_tob[int(0.025 * counts)]
 upperlimit_tob = final_tob[int(0.975 * counts)]
 
 # Calculate Mean and Standard Deviation
-print("Mean quality years gained (CPB): " + str((np.mean(Lfinal_tob))))
-print("Standard deviation: " + str((np.std(Lfinal_tob))))
+print("Mean quality years gained (CPB): " + str((np.mean(final_tob))))
+print("Standard deviation: " + str((np.std(final_tob))))
 print("Confidence interval: [" + str((lowerlimit_tob)) + ", " + str((upperlimit_tob)) + "]")
-print("Number of simulations: " + str(len(Lfinal_tob)))
+print("Number of simulations: " + str(len(final_tob)))
 
 # Build Histogram and Graph
 bins = np.arange(0, 0.0035, 0.000005)  # fixed bin size
 plt.xlim([0, 0.0035])
-plt.hist(Lfinal_alc, bins=bins, alpha=0.5)
-plt.hist(Lfinal_tob, bins=bins, alpha=0.5)
+plt.hist(final_alc, bins=bins, alpha=0.5)
+plt.hist(final_tob, bins=bins, alpha=0.5)
 plt.title('Histogram results')
 plt.xlabel('Calculated quality/intervention')
 plt.ylabel('Frequency (arb. units)')
@@ -147,7 +147,7 @@ print("Monetary value (Stdev): ", qual_year_stdev_alc * quality_years_alc_dollar
 print("\n\nTobbaco")
 print("patient volume:", patient_volume_tob)
 print("Fluctuation", Fluctuation_tob)
-print("Mean QALYS/year: ", qual_year_mean_tob)
+print("Mean QAL/year: ", qual_year_mean_tob)
 print("Quality_Stdev/year: ", qual_year_stdev_tob)
 print("Monetary value (Mean): ", qual_year_mean_tob * quality_years_tob_dollars)
 print("Monetary value (Stdev): ", qual_year_stdev_tob * quality_years_tob_dollars)
